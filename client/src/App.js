9895560d24f98b1a72bf5646eeaa7377bc470ad3
govindhansv <govindhansv333@gmail.com>
2022-05-04 10:57:49 +0530

    new

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Niravu website coming soon...</h1>
+        <img src={logo} className="App-logo" alt="The Bitch" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
